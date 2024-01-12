21a2edc15f2049de5d2aa81a88b57af7d4d2df49
Hector <programadorjr@coorsamexico.com>
2024-01-12 17:03:09 -0600

    skip rows empty

--- app/Imports/ImportBolo.php
+++ app/Imports/ImportBolo.php
@@ -20,42 +20,42 @@ class ImportBolo implements ToModel, WithHeadingRow
     {
         //Primero creamos los clientes si ya existen lo tomamos
         $cliente = cliente::select('clientes.*')
-        ->where('nombre', $row['cliente'])
+        ->where('nombre', $row['CLIENTE'])
         ->first();
         //Buscamos el destino si existe sino lo creamos
         $destino = destino::select('destinos.*')
-        ->where('nombre','=',$row['destino'])
+        ->where('nombre','=',$row['DESTINO'])
         ->first();
         //Buscamos el stage si esxite lo tomamos sino los creamos
         $stage = stage::select('stages.*')
-        ->where('nombre', '=' ,$row['stage'])
+        ->where('nombre', '=' ,$row['STAGE'])
         ->first();
 
         if($cliente == null) //sino existe lo creamos el cliente
         {
            $cliente = cliente::create([
-            'nombre' => $row['cliente']
+            'nombre' => $row['CLIENTE']
            ]);
         }
 
         if($destino == null) //sino existe lo creamos el destino
         {
            $destino = destino::create([
-            'nombre' => $row['destino']
+            'nombre' => $row['DESTINO']
            ]);
         }
 
         if($stage == null) //sino existe lo creamos el stage
         {
            $stage = stage::create([
-             'nombre' => $row['stage'],
+             'nombre' => $row['STAGE'],
              'categoria_stage_id' => 1
            ]);
         }
 
         //una vez creados o tomados creamos los dts 
         dt::updateOrCreate([
-           'referencia' => $row['dt'],
+           'referencia' => $row['LOAD'],
            'cliente_id' => $cliente['id'],
            'stage_id' => $stage['id'],
            'destino_id' => $destino['id']
