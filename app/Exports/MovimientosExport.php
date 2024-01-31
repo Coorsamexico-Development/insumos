65f793e548e1fdfa8ab40e8d4598770f9ec5d7ba
Hector <programadorjr@coorsamexico.com>
2024-01-31 08:29:19 -0600

    excel return

--- app/Exports/MovimientosExport.php
+++ app/Exports/MovimientosExport.php
@@ -2,12 +2,14 @@
 
 namespace App\Exports;
 
+use App\Models\categorias_producto;
 use App\Models\entrada;
+use App\Models\salidas;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class MovimientosExport implements FromCollection
+class MovimientosExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -22,7 +24,48 @@ class MovimientosExport implements FromCollection
     public function collection()
     {
         //
-        return entrada::all();
+        $categoria_producto = categorias_producto::select('categorias_productos.*')
+        ->where('categorias_productos.categoria_id','=',$this->categoria)
+        ->first();
+ 
+        $entradas = entrada::select('productos.nombre','entradas.cantidad','entradas.fecha')
+        ->join('categorias_productos','entradas.categorias_producto_id','categorias_productos.id')
+        ->join('productos','categorias_productos.producto_id','productos.id')
+        ->where('entradas.categorias_producto_id','=',$categoria_producto['id'])
+        ->orderBy('entradas.created_at', 'DESC')
+        ->get();
+ 
+        $salidas = salidas::select('productos.nombre','salidas.cantidad','salidas.created_at')
+        ->join('categorias_productos','salidas.categorias_producto_id','categorias_productos.id')
+        ->join('productos','categorias_productos.producto_id','productos.id')
+        ->where('salidas.categorias_producto_id','=',$categoria_producto['id'])
+        ->orderBy('salidas.created_at','DESC')
+        ->get();
+
+
+       $movimientos = [];
+
+       for ($i=0; $i < count($entradas) ; $i++) 
+       { 
+           $entradas[$i]->tipo ='entrada';
+          array_push($movimientos, $entradas[$i]);
+       }
+
+       for ($x=0; $x < count($salidas) ; $x++) 
+       { 
+           $salidas[$x]->tipo = 'salida';
+           array_push($movimientos, $salidas[$x]);
+       }
+   
+       usort($movimientos, function ($a, $b) 
+       {
+           return strcmp($b["created_at"], $a["created_at"]);
+       });
+
+
+
+       return collect($movimientos);
+
     }
 
     public function headings(): array
@@ -30,7 +73,9 @@ class MovimientosExport implements FromCollection
         return 
         [
           "Producto",
-          "Tipo de movimiento"
+          "Cantidad",
+          "Fecha",
+          "Tipo de movimiento",
         ];
     }
 }
--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Exports\MovimientosExport;
 use App\Models\categoria;
+use App\Models\categorias_producto;
+use App\Models\entrada;
+use App\Models\salidas;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -67,6 +70,46 @@ class CategoriaController extends Controller
 
     public function movimientosExport (Request $request)
     {
+/*
+        $categoria_producto = categorias_producto::select('categorias_productos.*')
+        ->where('categorias_productos.categoria_id','=',$request['categoria'])
+        ->first();
+ 
+        $entradas = entrada::select('productos.nombre','entradas.cantidad','entradas.fecha')
+        ->join('categorias_productos','entradas.categorias_producto_id','categorias_productos.id')
+        ->join('productos','categorias_productos.producto_id','productos.id')
+        ->where('entradas.categorias_producto_id','=',$categoria_producto['id'])
+        ->orderBy('entradas.created_at', 'DESC')
+        ->get();
+ 
+        $salidas = salidas::select('productos.nombre','salidas.cantidad','salidas.created_at')
+        ->join('categorias_productos','salidas.categorias_producto_id','categorias_productos.id')
+        ->join('productos','categorias_productos.producto_id','productos.id')
+        ->where('salidas.categorias_producto_id','=',$categoria_producto['id'])
+        ->orderBy('salidas.created_at','DESC')
+        ->get();
+
+        $movimientos = [];
+
+        for ($i=0; $i < count($entradas) ; $i++) 
+        { 
+            $entradas[$i]->tipo ='entrada';
+           array_push($movimientos, $entradas[$i]);
+        }
+
+        for ($x=0; $x < count($salidas) ; $x++) 
+        { 
+            $salidas[$x]->tipo = 'salida';
+            array_push($movimientos, $salidas[$x]);
+        }
+    
+        usort($movimientos, function ($a, $b) 
+        {
+            return strcmp($b["created_at"], $a["created_at"]);
+        });
+
+        return $movimientos;
+*/
         return Excel::download(new MovimientosExport($request['categoria']), 'reporte.xlsx');
     }
 }
