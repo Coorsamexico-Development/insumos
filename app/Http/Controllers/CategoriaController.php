dc1ee3ed6574350da87336a5e81f2a878a6e908d
Hector <programadorjr@coorsamexico.com>
2024-02-01 09:45:39 -0600

    check

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -70,54 +70,6 @@ class CategoriaController extends Controller
 
     public function movimientosExport (Request $request)
     {
-        /*
-        $categorias_productos = categorias_producto::select('categorias_productos.*')
-         ->where('categorias_productos.categoria_id','=',$request['categoria'])
-         ->get();
-
-        $movimientos = [];
-
-        //return $categorias_productos[0];
-
-        for ($i=0; $i < count($categorias_productos) ; $i++) 
-        { 
-            $categoria_producto = $categorias_productos[$i];
- 
-            $entradas = entrada::select('productos.nombre','entradas.cantidad','entradas.fecha')
-            ->join('categorias_productos','entradas.categorias_producto_id','categorias_productos.id')
-            ->join('productos','categorias_productos.producto_id','productos.id')
-            ->where('entradas.categorias_producto_id','=',$categoria_producto['id'])
-            ->orderBy('entradas.created_at', 'DESC')
-            ->get();
-     
-            $salidas = salidas::select('productos.nombre','salidas.cantidad','salidas.created_at')
-            ->join('categorias_productos','salidas.categorias_producto_id','categorias_productos.id')
-            ->join('productos','categorias_productos.producto_id','productos.id')
-            ->where('salidas.categorias_producto_id','=',$categoria_producto['id'])
-            ->orderBy('salidas.created_at','DESC')
-            ->get();
-
-            for ($x=0; $x < count($entradas) ; $x++) 
-            { 
-                $entradas[$x]->tipo ='entrada';
-               array_push($movimientos, $entradas[$x]);
-            }
-     
-            for ($y=0; $y < count($salidas) ; $y++) 
-            { 
-                $salidas[$y]->tipo = 'salida';
-                array_push($movimientos, $salidas[$y]);
-            }
-        
-        }
-
- 
-       usort($movimientos, function ($a, $b) 
-       {
-           return strcmp($b["created_at"], $a["created_at"]);
-       });
-
-      */
         return Excel::download(new MovimientosExport($request['categoria']), 'reporte.xlsx');
     }
 }
