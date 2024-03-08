80b02fc6df43714cf625e4bf24a8854d1c58599b
Hector <programadorjr@coorsamexico.com>
2024-03-08 08:50:42 -0600

    change prox reestock date

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -525,7 +525,7 @@ const closeModalGraph = () =>
                                             </div>
                                           </td>
                                           <td class="py-2 text-center">
-                                            {{ new Date().getFullYear()+'-'+ new Date().getMonth()+'-'+(new Date().getDay()+2) }}
+                                            {{ new Date().getFullYear()+'-'+ new Date().getMonth()+'-'+(new Date().getDay()+producto.tiempo_de_reestock) }}
                                           </td>
                                           <td class="py-2 text-center">
                                             <p class="text-[#121A3C]" v-if="producto.corte_diario">
